7d803bf488a2c8c1021f4ce27a9a28ad3aa37e6f
WonjunJang <wonjun84@naver.com>
2021-04-11 14:48:56 +0900

    feat: Add config/index.js

---
+++ Server/config/index.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  port: process.env.PORT,
+  databaseURL: process.env.CONNECTION_URL,
+  accessKey: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_ACCESS_SECRET,
+  region: process.env.S3_REGION,
+  bucketName: process.env.S3_BUCKET_NAME,
+};
