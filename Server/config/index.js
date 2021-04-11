06553d5c97f5fdbcd6b9bca70bc3500126bc6d29
WonjunJang <wonjun84@naver.com>
2021-04-11 15:09:24 +0900

    feat: Add utility functions

--- Server/config/index.js
+++ Server/config/index.js
@@ -7,6 +7,6 @@ module.exports = {
   databaseURL: process.env.CONNECTION_URL,
   accessKey: process.env.S3_ACCESS_KEY,
   secretAccessKey: process.env.S3_ACCESS_SECRET,
-  region: process.env.S3_REGION,
+  region: process.env.S3_BUCKET_REGION,
   bucketName: process.env.S3_BUCKET_NAME,
 };
