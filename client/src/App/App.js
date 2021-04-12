96b4082a86a6fa060bae2813817079ca4212c361
ejinaaa <eunjink0423@gmail.com>
2021-04-12 20:52:12 +0900

    test: Test heading component

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,6 +1,11 @@
+import { Heading } from 'components';
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Heading level={1} content="오늘 기분타? 그럼 산타!" />
+    </div>
+  );
 }
 
 export default App;
