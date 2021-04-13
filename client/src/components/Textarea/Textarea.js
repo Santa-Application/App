edf8ce442c5aac36a9b0d2916cf35ad763e7deaf
ejinaaa <eunjink0423@gmail.com>
2021-04-13 21:42:21 +0900

    feat: Add onChage property of textarea component

--- client/src/components/Textarea/Textarea.js
+++ client/src/components/Textarea/Textarea.js
@@ -2,21 +2,25 @@ import PropTypes from 'prop-types';
 import { textarea } from './Textarea.module.scss';
 import classNames from 'classnames';
 
-const Textarea = ({ value, className, ...restProps }) => {
+const Textarea = ({ value, onChange, className, ...restProps }) => {
   const textareaClasses = classNames(className, textarea);
 
   return (
     <label>
-      <textarea className={textareaClasses}>{value}</textarea>
+      <textarea onChange={onChange} className={textareaClasses}>
+        {value}
+      </textarea>
     </label>
   );
 };
 
 Textarea.defaultProps = {
   value: '',
+  onChange: null,
 };
 Textarea.propTypes = {
   value: PropTypes.string,
+  onChange: PropTypes.oneOfType([PropTypes.func, PropTypes.oneOf([null])]),
 };
 
 export default Textarea;
