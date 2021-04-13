016dbf17d40f1d1489dc4625c693c760965061b8
ejinaaa <eunjink0423@gmail.com>
2021-04-13 20:19:30 +0900

    docs: Remove className property in propTypes of Heading.js

--- client/src/components/Heading/Heading.js
+++ client/src/components/Heading/Heading.js
@@ -11,12 +11,10 @@ const Heading = ({ level, className, content, ...restProps }) => {
 
 Heading.defaultProps = {
   level: 2,
-  className: '',
   content: '',
 };
 Heading.propTypes = {
   level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]).isRequired,
-  className: PropTypes.string,
   content: PropTypes.string,
 };
 
