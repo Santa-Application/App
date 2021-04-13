c835d5c6cb317bcc103a7e61f1ad95de0b44051b
Bernese-Corgi <jnyng0621@gmail.com>
2021-04-13 22:16:23 +0900

    feat: Create SelectBox component'

---
+++ client/src/components/SelectBox/SelectBox.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import Icon from 'components/Icon/Icon';
+import {
+  selectBoxContainer,
+  selectBoxButton,
+  selectBoxList,
+} from './SelectBox.module.scss';
+import { string, bool, func, array } from 'prop-types';
+import classNames from 'classnames';
+
+const SelectBox = ({
+  className,
+  isOpened,
+  selectItem,
+  handleOpenSelectBox,
+  handleClickItem,
+  // data,
+}) => {
+  const containerClasses = classNames(className, selectBoxContainer);
+  const selectButtonClasses = classNames(className, selectBoxButton);
+  const selectListClasses = classNames(className, selectBoxList);
+  // mock data
+  const data = [
+    '최우영산',
+    '김데레사산',
+    '이웅모산',
+    '야무산',
+    'devLee산',
+    '박두진산',
+    '신제용산',
+    '최우영산',
+    '김데레사산',
+    '이웅모산',
+    '야무산',
+    'devLee산',
+    '박두진산',
+    '신제용산',
+  ];
+
+  // const [isOpened, setIsOpened] = useState(false);
+  // const [selectItem, setSelectItem] = useState('select title');
+
+  // const handleOpenSelectBox = () => {
+  //   setIsOpened(!isOpened);
+  // };
+
+  // const handleClickItem = e => {
+  //   setSelectItem(e.target.textContent);
+  //   setIsOpened(false);
+  // };
+
+  return (
+    <div className={containerClasses} role="listbox" tabIndex="1">
+      <button className={selectButtonClasses} onClick={handleOpenSelectBox}>
+        <p>{selectItem}</p>
+        <Icon shape={isOpened ? 'selectClose' : 'selectOpen'} />
+      </button>
+      {isOpened && (
+        <ul className={selectListClasses}>
+          {data.map(item => {
+            return (
+              <li>
+                <button type="button" onClick={handleClickItem} role="option">
+                  {item}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+SelectBox.defaultProps = {
+  className: '',
+  isOpened: false,
+  selectItem: '',
+  handleOpenSelectBox: null,
+  handleClickItem: null,
+  data: [],
+};
+
+SelectBox.propTypes = {
+  className: string,
+  isOpened: bool,
+  selectItem: string.isRequired,
+  handleOpenSelectBox: func,
+  handleClickItem: func,
+  data: array,
+};
+
+export default SelectBox;
