0fd8a57fdf7916bcfa73f78929d6760df18349a2
WonjunJang <wonjun84@naver.com>
2021-04-13 17:51:02 +0900

    docs: Add required attribute to date in regularPostSchema

--- Server/models/RegularPost.js
+++ Server/models/RegularPost.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const regularPostSchema = Schema({
   publisher: { type: String, required: true },
   imageURL: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  date: { type: Date, default: Date.now , required: true },
   content: { type: String, required: true },
   mountain: { type: String, required: true },
   views: { type: Number, default: 0 },
