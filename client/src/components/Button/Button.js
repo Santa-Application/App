33026c288af6dba1ff8623720d9f29a367091c83
WonjunJang <wonjun84@naver.com>
2021-04-13 12:03:58 +0900

    feat: Make button component

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import classNames from 'classnames';
+import { string, bool, node, func } from 'prop-types';
+import {
+  button,
+  primary,
+  secondary as secondaryClass
+} from './Button.module.scss';
 
-const Button = () => {
-  return (
-    <div>
-      
-    </div>
+const Button = ({
+  secondary,
+  type, 
+  disabled, 
+  value, 
+  className,
+  onClick,
+  ...restProps
+}) => {
+
+  const composeClasses = classNames(
+    button,
+    secondary ? secondaryClass : primary
   )
-}
 
-export default Button
+
+
+  return (
+    <button
+      className={classNames(composeClasses, className)}
+      type={type}
+      disabled={disabled}
+      value={value}
+      onClick={onClick}
+      {...restProps}
+    >
+      {restProps.children}
+    </button>
+  );
+};
+
+export default Button;
+
+Button.defaultProps = {
+  secondary: false,
+  type: 'submit',
+  disabled: false,
+  value: 'Default submit button',
+  children: 'Button',
+  className: '',
+};
+
+Button.propTypes = {
+  secondary: bool,
+  type: string,
+  disabled: bool,
+  value: string,
+  chidren: node,
+  className: string,
+  onClick: func
+};
