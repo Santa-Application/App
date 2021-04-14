cdb1adf2b21bc5b95d11dd798b9cf2cffe7cb6db
ejinaaa <eunjink0423@gmail.com>
2021-04-14 21:20:50 +0900

    feat: Create utility function about post date

--- client/src/utils/postDate.js
+++ client/src/utils/postDate.js
@@ -13,3 +13,15 @@ export const getPostDateInKorean = postDate => {
 
   return `${year}년 ${month}월 ${date}일`;
 };
+
+export const getPostDateObject = postDate => {
+  const year = postDate.getFullYear();
+  const month = postDate.getMonth() + 1;
+  const date = postDate.getDate();
+
+  return {
+    year,
+    month,
+    date,
+  };
+};
