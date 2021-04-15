12fa363d57f698890e9319879dcb1491a8356002
WonjunJang <wonjun84@naver.com>
2021-04-15 14:58:27 +0900

    fix: Modify regularPost schema

--- Server/models/RegularPost.js
+++ Server/models/RegularPost.js
@@ -1,13 +1,14 @@
 const { Schema, model } = require('mongoose');
 
 const regularPostSchema = Schema({
-  publisher: { type: String, required: true },
-  imageURL: { type: String, required: true },
-  date: { type: Date, default: Date.now , required: true },
-  content: { type: String, required: true },
-  mountain: { type: String, required: true },
+  publisherId: { type: String, required: true },
+  publisherName: { type: String, required: true },
+  postingDate: { type: Date, default: Date.now , required: true },
   views: { type: Number, default: 0 },
   likes: { types: Number, default: 0 },
+  mountainName: { type: String, required: true },
+  imageURL: { type: String, required: true },
+  content: { type: String, required: true },
 });
 
 module.exports = model('RegularPost', regularPostSchema);
