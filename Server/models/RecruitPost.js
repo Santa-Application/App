81be8c18a7401e3a0117a80036635cc4bdcef920
WonjunJang <wonjun84@naver.com>
2021-04-15 14:55:28 +0900

    fix: modify RecruitPost schema

--- Server/models/RecruitPost.js
+++ Server/models/RecruitPost.js
@@ -1,14 +1,17 @@
 const { Schema, model } = require('mongoose');
 
 const recruitPostSchema = Schema({
-  mountainName: { type: String, required: true },
-  recruitingNumber: { type: Number, required: true },
-  recruitingLevels: [{ type: Number, required: true }],
-  recruitingSex: { type: String, required: true },
-  recruitingAge: Number,
-  date: { type: Date, default: Date.now, required: true },
-  description: String,
+  publisherId: { type: String, required: true },
+  publisherName: { type: String, required: true },
+  postingDate: { type: Date, default: Date.now, required: true },
   views: { type: Number, default: 0 },
+  mountainName: { type: String, required: true },
+  recruitingDate: { type: Date, required: true },
+  recruitingLevels: [{ type: String, required: true }],
+  recruitingGender: { type: String, required: true },
+  recruitingAge: { type: Number, required: true },
+  recruitingNumber: { type: Number, required: true },
+  description: String,
 });
 
 module.exports = model('RecruitPost', recruitPostSchema);
