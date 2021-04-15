c9d948e14165aa241a26daf00c6a767294e732ae
ejinaaa <eunjink0423@gmail.com>
2021-04-15 11:43:39 +0900

    docs: Rename prop text to labelText of radio button component

--- client/src/components/RadioButton/RadioButton.js
+++ client/src/components/RadioButton/RadioButton.js
@@ -16,17 +16,17 @@ const RadioButton = ({
   className: { label: labelClassName },
   ...restProps
 }) => {
-  let text = '';
+  let labelText = '';
   switch (type) {
     case 'female':
-      text = '여성';
+      labelText = '여성';
       break;
     case 'male':
-      text = '남성';
+      labelText = '남성';
       break;
     case 'genderBoth':
     default:
-      text = '상관없음';
+      labelText = '상관없음';
   }
 
   const iconSizeWithUnit = `${iconSize}rem`;
@@ -50,7 +50,7 @@ const RadioButton = ({
           width={iconSizeWithUnit}
           height={iconSizeWithUnit}
         />
-        {text}
+        {labelText}
         <Icon shape={type} />
       </label>
     </div>
