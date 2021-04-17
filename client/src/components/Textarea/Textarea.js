f079981ed68a30e629a2d3758d4033f414c19cff
ejinaaa <eunjink0423@gmail.com>
2021-04-18 02:53:58 +0900

    feat: Add id prop in textarea component

--- client/src/components/Textarea/Textarea.js
+++ client/src/components/Textarea/Textarea.js
@@ -2,31 +2,35 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { textarea } from './Textarea.module.scss';
 
-const Textarea = ({ value, onChange, className, ...restProps }) => {
-  const textareaClasses = classNames(className, textarea);
+const Textarea = ({ id, value, onChange, className, ...restProps }) => {
+  const textareaClasses = classNames(className.textarea, textarea);
 
   return (
-    <label>
+    <>
+      <label htmlFor={id}></label>
       <textarea
+        id={id}
+        value={value}
         onChange={onChange}
         className={textareaClasses}
-        value={value}
       ></textarea>
-    </label>
+    </>
   );
 };
 
 Textarea.defaultProps = {
+  id: '',
   value: '',
   onChange: null,
-  className: '',
+  className: {},
 };
 
 Textarea.propTypes = {
+  id: PropTypes.string,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.oneOfType([PropTypes.func, PropTypes.oneOf([null])])
     .isRequired,
-  className: PropTypes.string,
+  className: PropTypes.object,
 };
 
 export default Textarea;
--- client/src/components/Textarea/Textarea.stories.js
+++ client/src/components/Textarea/Textarea.stories.js
@@ -4,6 +4,11 @@ export default {
   title: 'Components/Textarea',
   component: Textarea,
   argTypes: {
+    id: {
+      table: {
+        disable: true,
+      },
+    },
     value: {
       type: { name: '사용자 입력 텍스트', required: true },
       description: '사용자가 입력한 텍스트를 전달받습니다.',
