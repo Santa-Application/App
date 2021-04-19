afc848520b32420f7d8906ed8c9766c1a50ded31
WonjunJang <wonjun84@naver.com>
2021-04-19 09:34:11 +0900

    feat: Add mountain page APIs

--- Server/routes/mountain.js
+++ Server/routes/mountain.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const { isError } = require('joi');
 const jwt = require('jsonwebtoken');
-const Mountain = require('../models/Mountain');
+const { Mountain, RegularPost, RecruitPost } = require('../models/index');
 const { downloadFile } = require('../utils/s3');
 
 // get all the Mountain Information
@@ -13,7 +14,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get Mountain Information
+// get Mountain Information => overviews
 router.get('/:mountainName', async (req, res) => {
   try {
     // find the mountain
@@ -26,4 +27,41 @@ router.get('/:mountainName', async (req, res) => {
   }
 });
 
+// get Mountain Reviews
+router.get('/:mountainName/reviews', async (req, res) => {
+  // find all reviews with the mountain:)
+  try {
+    const allPosts = await RegularPost.find({ mountain: req.params.mountainName });
+    // for all regularPosts, get a limited URL that is accessible
+    // and put it in the payload and send
+    const datas = allPosts.map(async (post) => {
+      // get URL
+      const imageURL = await downloadFile(post.imageURL);
+
+      return { post, imageURL };
+    });
+
+    res.status(200).send(datas);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// get Mountain Recruits
+router.get('/:mountainName/recruits', async (req, res) => {
+  try {
+    const allPosts = await RecruitPost.find({ mountainName: req.params.mountainName });
+
+    const datas = allPosts.map(async (post) => {
+      // recruit post publisher imageURL
+      const recruiterProfileImageURL = await downloadFile(post.recruiterID);
+
+      return { post, recruiterProfileImageURL };
+    });
+
+    res.status(200).send(datas);
+  } catch (err) {
+    res.send(err);
+  }
+});
 module.exports = router;
