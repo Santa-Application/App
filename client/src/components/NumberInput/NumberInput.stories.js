fa836799d60cb7058d7cfb55bc0e07cd731e4ba2
Bernese-Corgi <jnyng0621@gmail.com>
2021-04-21 10:55:08 +0900

    fix: Delete name props in NumberInput compoenent's storybook.

--- client/src/components/NumberInput/NumberInput.stories.js
+++ client/src/components/NumberInput/NumberInput.stories.js
@@ -15,12 +15,12 @@ export default {
       type: 'string',
       description: '식별 가능한 id값을 필수로 전달합니다.',
     },
-    name: {
-      type: 'string',
-      description:
-        '폼 컨트롤 시, 사용자가 입력한 값과 매칭되는 네임 값을 설정합니다.',
-    },
-    content: {
+    // name: {
+    //   type: 'string',
+    //   description:
+    //     '폼 컨트롤 시, 사용자가 입력한 값과 매칭되는 네임 값을 설정합니다.',
+    // },
+    unit: {
       type: 'string',
       description: 'input 옆에 표시되는 단위를 전달받습니다.',
     },
