db702ffb2a1dd37b5ac5331203551d4bc69094c1
ejinaaa <eunjink0423@gmail.com>
2021-04-22 02:34:41 +0900

    feat: Update component to use formik

--- client/src/components/FileInput/FileInput.js
+++ client/src/components/FileInput/FileInput.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import axios from 'axios';
 import classNames from 'classnames';
 import PropTypes, { string } from 'prop-types';
 import { fileInput } from './FileInput.module.scss';
 
 const FileInput = ({ field, ...inputProps }) => {
-  const { id, label, className, fileRoute, onChange } = inputProps;
+  const { id, label, className, fileRoute, setFieldValue } = inputProps;
   const FileInputClasses = classNames(className, fileInput);
   /* ----------------------------------- 
     전달될 상태와 핸들러.
@@ -18,6 +19,27 @@ const FileInput = ({ field, ...inputProps }) => {
   };
   --------------------------------------- */
 
+  // const uploadImage = async imageFile => {
+  //   const imageUrlData = new FormData();
+  //   imageUrlData.append(imageFile);
+  //   try {
+  //     const response = await axios.post('', imageUrlData, {
+  //       headers: {
+  //         'Content-type': 'multipart/form-data',
+  //       },
+  //     });
+
+  //     return response.data;
+  //   } catch (e) {
+  //     throw Error(e);
+  //   }
+  // };
+
+  // const handleChangeFileInput = async e => {
+  //   const imagePath = await uploadImage(e.target.file[0]);
+  //   setFieldValue(imagePath);
+  // };
+
   return (
     <div className={FileInputClasses}>
       <label htmlFor={id}>
@@ -27,7 +49,8 @@ const FileInput = ({ field, ...inputProps }) => {
           type="file"
           min="1"
           accept=".gif, .jpg, .png"
-          onChange={onChange}
+          {...field}
+          // onChange={handleChangeFileInput}
         />
       </label>
       <span children={fileRoute}></span>
