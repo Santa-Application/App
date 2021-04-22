ae96db57ad8b7479b85aa545a424021cafbc96ea
ejinaaa <eunjink0423@gmail.com>
2021-04-23 06:02:31 +0900

    fix: Fix bug in select box

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as RegisterForm } from './RegisterForm/RegisterForm';
 export { default as RegularPostForm } from './RegularPostForm/RegularPostForm';
 export { default as SignInForm } from './SignInForm/SignInForm';
+export { default as RecruitForm } from './RecruitForm/RecruitForm';
