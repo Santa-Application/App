328974288b4c87b27b49a4b1f3c3fbc0dde74d0a
ejinaaa <eunjink0423@gmail.com>
2021-04-23 06:11:16 +0900

    feat: Add redux store provider and wrap app component

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+import StoreProvider from 'redux/store';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <StoreProvider>
+      <Router>
+        <App />
+      </Router>
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
