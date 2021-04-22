01d84b011188909d3bd189ffa882d4fbdb47ca34
Bernese-Corgi <jnyng0621@gmail.com>
2021-04-22 23:43:20 +0900

    feat: Create Recruit Form component.

--- client/src/containers/RecruitForm/RecruitForm.js
+++ client/src/containers/RecruitForm/RecruitForm.js
@@ -1,7 +1,7 @@
 import FormItem from 'components/FormItem/FormItem';
 import { Form, Formik } from 'formik';
-import React, { useState } from 'react';
-import { formHandler } from 'utils/';
+import React, { useEffect, useState } from 'react';
+import { formHandler, validationSchema } from 'utils/';
 import top100Mountains from 'data/top100Mountains';
 import { Button } from 'components';
 
@@ -12,12 +12,13 @@ const RecruitForm = () => {
     handleFocusSelectBoxInput,
     handleClickSelectBoxInputButton,
     handleClickSelectBoxListItemButton,
-    handleChangeSelectBoxInput,
+    // handleChangeSelectBoxInput,
+    handleBlurSelectBoxInput,
+    handleChangeFileInput,
   } = formHandler;
 
-  const [selectedDate, setSelectedDate] = useState(new Date());
   const [isSelectBoxOpened, setIsSelectBoxOpened] = useState(false);
-  const [mountainNameInput, setMountainNameInput] = useState('등산할 산');
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
     <Formik
@@ -27,174 +28,183 @@ const RecruitForm = () => {
         recruitingLevels: [],
         recruitingSex: '',
         recruitingAge: [],
-        description: '',
-        views: 0,
-        recruiterID: '',
-        recruitees: '',
-        recruitDate: '',
+        // numberMin: 20,
+        // numberMax: 45,
+        // description: '',
+        // views: 0,
+        // recruiterID: '',
+        // recruitees: '',
+        recruitDate: {},
         title: '',
+        // imageURL: {},
       }}
       onSubmit={values => {
         console.log(values);
       }}
     >
-      <Form>
-        <FormItem
-          headingProps={{
-            level: 3,
-            content: '제목',
-          }}
-          descProps={{
-            content: '매력적인 제목으로 메이트에게 어필해보세요',
-          }}
-          inputProps={{
-            formType: 'text',
-            type: 'text',
-            id: 'title',
-            name: 'title',
-          }}
-        />
-        <FormItem
-          headingProps={{
-            level: 3,
-            content: '등산할 산',
-          }}
-          descProps={{
-            content: '등산할 산을 지정해주세요',
-          }}
-          inputProps={{
-            formType: 'select',
-            id: 'mountainName',
-            name: 'mountainName',
-            isOpened: isSelectBoxOpened,
-            inputValue: mountainNameInput,
-            datas: top100Mountains,
-            onFocus: e => handleFocusSelectBoxInput(e, setIsSelectBoxOpened),
-            onClickInputButton: e =>
-              handleClickSelectBoxInputButton(
-                e,
-                setIsSelectBoxOpened,
-                isSelectBoxOpened
-              ),
-            onClickListButton: e =>
-              handleClickSelectBoxListItemButton(
-                e,
-                setMountainNameInput,
-                setIsSelectBoxOpened
-              ),
-            onChange: e =>
-              handleChangeSelectBoxInput(
-                e,
-                setMountainNameInput,
-                setIsSelectBoxOpened
-              ),
-          }}
-        />
-        <FormItem
-          headingProps={{
-            level: 3,
-            content: '등산 날짜',
-          }}
-          descProps={{
-            content: '등산하고 싶은 날짜를 선택해 주세요',
-          }}
-          inputProps={{
-            formType: 'date',
-            id: 'recruitDate',
-            name: 'recruitDate',
-            selectedDate: selectedDate,
-            onSelect: date => handleSelectDate(date, setSelectedDate),
-            onFocus: e => handleFocusAllInput(e),
-          }}
-        />
-        {/* <FormItem
-          headingProps={{
-            level: 3,
-            content: '등산 레벨',
-          }}
-          descProps={{
-            content:
-              '같이 등산하고 싶은 메이트의 등산 레벨을 지정해주세요 (중복 가능)',
-          }}
-          inputProps={{
-            formType: '',
-            id: '',
-            name: '',
-          }}
-          /> */}
-        <FormItem
-          headingProps={{
-            level: 3,
-            content: '등산 메이트 성별',
-          }}
-          descProps={{
-            content: '같이 등산하고 싶은 메이트의 성별을 지정해주세요',
-          }}
-          inputProps={{
-            formType: 'radio',
-            id: 'recruitingAge',
-            name: 'recruitingAge',
-          }}
-        />
-        {/* <FormItem
-          headingProps={{
-            level: 3,
-            content: '',
-          }}
-          descProps={{
-            content: '',
-          }}
-          inputProps={{
-            formType: '',
-            id: '',
-            name: '',
-          }}
-        /> */}
-        {/* <FormItem
-          headingProps={{
-            level: 3,
-            content: '',
-          }}
-          descProps={{
-            content: '',
-          }}
-          inputProps={{
-            formType: '',
-            id: '',
-            name: '',
-          }}
-        /> */}
-        {/* <FormItem
-          headingProps={{
-            level: 3,
-            content: '',
-          }}
-          descProps={{
-            content: '',
-          }}
-          inputProps={{
-            formType: '',
-            id: '',
-            name: '',
-          }}
-        /> */}
-        {/* <FormItem
-          headingProps={{
-            level: 3,
-            content: '',
-          }}
-          descProps={{
-            content: '',
-          }}
-          inputProps={{
-            formType: '',
-            id: '',
-            name: '',
-          }}
-        /> */}
-        <Button type="button" secondary="true" children="취소하기" />
-        <Button type="submit" children="등록하기" />
-      </Form>
+      {({ setFieldValue }) => {
+        return (
+          <Form>
+            <FormItem
+              headingProps={{
+                level: 3,
+                content: '제목',
+              }}
+              descProps={{
+                content: '매력적인 제목으로 메이트에게 어필해보세요',
+              }}
+              inputProps={{
+                formType: 'text',
+                type: 'text',
+                id: 'title',
+                name: 'title',
+              }}
+            />
+            <FormItem
+              headingProps={{
+                level: 3,
+                content: '등산할 산',
+              }}
+              descProps={{
+                content: '등산할 산을 지정해주세요',
+              }}
+              inputProps={{
+                formType: 'select',
+                id: 'mountainName',
+                name: 'mountainName',
+                isOpened: isSelectBoxOpened,
+                setIsOpened: setIsSelectBoxOpened,
+                placeholder: '등산할 산',
+                datas: top100Mountains,
+                onFocus: e =>
+                  handleFocusSelectBoxInput(e, setIsSelectBoxOpened),
+                // onBlur: handleBlur,
+                onClickInputButton: e =>
+                  handleClickSelectBoxInputButton(
+                    e,
+                    setIsSelectBoxOpened,
+                    isSelectBoxOpened
+                  ),
+                onClickListButton: e =>
+                  handleClickSelectBoxListItemButton(
+                    e,
+                    'mountainName',
+                    setFieldValue,
+                    setIsSelectBoxOpened
+                  ),
+              }}
+            />
+            <FormItem
+              headingProps={{
+                level: 3,
+                content: '등산 날짜',
+              }}
+              descProps={{
+                content: '등산하고 싶은 날짜를 선택해 주세요',
+              }}
+              inputProps={{
+                formType: 'date',
+                id: 'recruitDate',
+                name: 'recruitDate',
+                selectedDate: selectedDate,
+                setSelectedDate: setSelectedDate,
+                setFieldValue,
+                onSelect: handleSelectDate,
+                onFocus: handleFocusAllInput,
+              }}
+            />
+            <FormItem
+              headingProps={{
+                level: 3,
+                content: '등산 메이트 모집 인원',
+              }}
+              descProps={{
+                content: '같이 등산하고 싶은 메이트의 인원을 지정해주세요',
+              }}
+              inputProps={{
+                formType: 'number',
+                id: 'recruitingNumber',
+                name: 'recruitingNumber',
+                unit: '명',
+              }}
+            />
+            <FormItem
+              headingProps={{
+                level: 3,
+                content: '등산 메이트 성별',
+              }}
+              descProps={{
+                content: '같이 등산하고 싶은 메이트의 성별을 지정해주세요',
+              }}
+              inputProps={{
+                formType: 'gender',
+                id: 'recruitingSex',
+                name: 'recruitingSex',
+              }}
+            />
+            <FormItem
+              headingProps={{
+                level: 3,
+                content: '등반 레벨',
+              }}
+              descProps={{
+                content: '등반 레벨',
+              }}
+              inputProps={{
+                formType: 'hikingLevel',
+                id: 'recruitingLevels',
+                name: 'recruitingLevels',
+              }}
+            />
+            <FormItem
+              headingProps={{
+                level: 3,
+                content: '이메일',
+              }}
+              descProps={{
+                content: '이메일 입력해주세요',
+              }}
+              inputProps={{
+                formType: 'text',
+                type: 'email',
+                id: 'email',
+                name: 'email',
+              }}
+            />
+            <FormItem
+              headingProps={{
+                level: 3,
+                content: '비밀번호',
+              }}
+              descProps={{
+                content: '비밀번호',
+              }}
+              inputProps={{
+                formType: 'text',
+                type: 'password',
+                id: 'password',
+                name: 'password',
+              }}
+            />
+            <FormItem
+              headingProps={{
+                level: 3,
+                content: 'textarea',
+              }}
+              descProps={{
+                content: 'textarea',
+              }}
+              inputProps={{
+                formType: 'textarea',
+                id: 'description',
+                name: 'description',
+              }}
+            />
+            <Button type="button" secondary children="취소하기" />
+            <Button type="submit" children="등록하기" />
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
