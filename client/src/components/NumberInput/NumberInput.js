404399fdedf6dfe60daacffbe3f2bae52320e12f
Bernese-Corgi <jnyng0621@gmail.com>
2021-04-23 17:29:47 +0900

    style: Fix style in NumberInput component.

--- client/src/components/NumberInput/NumberInput.js
+++ client/src/components/NumberInput/NumberInput.js
@@ -1,22 +1,62 @@
+/* eslint-disable indent */
 import React from 'react';
 import classNames from 'classnames';
 import { string } from 'prop-types';
 import { numberInput } from './NumberInput.module.scss';
 
 const NumberInput = ({ field, inputProps }) => {
-  const { id, className, unit } = inputProps;
+  const {
+    id,
+    name,
+    className,
+    unit,
+    handleChange,
+    onChangeMinInput,
+    onChangeMaxInput,
+    minInputName,
+    maxInputName,
+    setFieldValue,
+    currentAge,
+    setCurrentAge,
+  } = inputProps;
   const numberInputClasses = classNames(className, numberInput);
+
+  // const handleChangeInput = e => {
+  //   if (name === maxInputName) {
+  //     onChangeMaxInput(
+  //       e,
+  //       maxInputName,
+  //       setFieldValue,
+  //       currentAge,
+  //       setCurrentAge
+  //     );
+  //     return;
+  //   }
+  //   if (name === minInputName) {
+  //     onChangeMinInput(
+  //       e,
+  //       minInputName,
+  //       setFieldValue,
+  //       currentAge,
+  //       setCurrentAge
+  //     );
+  //     return;
+  //   }
+  //   handleChange(e);
+  // };
+
   return (
-    <>
+    <div className={numberInputClasses}>
       <input
         id={id}
+        name={name}
         type="number"
         min="1"
-        className={numberInputClasses}
-        {...field}
-      />{' '}
+        {...(name === 'recruitingAge' || { ...field })}
+        // onChange={handleChangeInput}
+      />
       <span>{unit}</span>
-    </>
+    </div>
   );
 };
 
