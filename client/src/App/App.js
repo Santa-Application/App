614aafe9cd4cd490bafacb2e1c567e25a4705754
ejinaaa <eunjink0423@gmail.com>
2021-04-24 21:48:10 +0900

    feat: Add heading style and organized the components

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,6 +1,6 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { HomePage, GlobalRecruitPost, GlobalRegularPost } from 'pages';
+import { HomePage, Login } from 'pages';
 import {
   Footer,
   Header,
@@ -8,47 +8,32 @@ import {
   RecruitPostDetail,
   RecruitPostList,
 } from 'components';
+import { pageHeading } from './App.module.scss';
 
 function App() {
   return (
     <div className="App">
       <HelmetProvider>
-        <Header />
+        <Route path="/" component={Header} />
         <main>
           <Route
             path="/recruit"
-            component={() => <Heading content="RECRUIT" />}
+            component={() => (
+              <Heading content="RECRUIT" className={pageHeading} />
+            )}
           />
           <Route path="/recruit" exact component={RecruitPostList} />
           <Route path="/recruit/:postId" exact component={RecruitPostDetail} />
           <Route
             path="/review"
-            component={() => <Heading content="REVIEW" />}
+            component={() => (
+              <Heading content="REVIEW" className={pageHeading} />
+            )}
           />
-          {/* <Route path="/review" exact component={GlobalRegularPostList} />
-            <Route
-            path="/review/edit"
-            exact
-            component={GlobalRegularPostEdit}
-            />
-            <Route
-            path="/review/create"
-            exact
-            component={GlobalRegularPostCreate}
-            />
-            <Route
-            path="/recruit/:postId"
-            exact
-            component={GlobalRecruitPostDetail}
-            />
-            <Route
-            path="/review/:postId"
-            exact
-            component={GlobalRegularPostDetail}
-          /> */}
-          <Route path="/" exact component={HomePage} />
+          <Route path="/login" exact component={Login} />
           <Switch></Switch>
         </main>
+        <Route path="/" exact component={HomePage} />
         <Footer />
       </HelmetProvider>
     </div>
