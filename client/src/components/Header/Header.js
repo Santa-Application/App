c9ed9b47afec34daba6e3fc968810e786ffc277c
ejinaaa <eunjink0423@gmail.com>
2021-04-24 21:52:16 +0900

    feat: Set router and add event handlers on buttons

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -19,14 +19,16 @@ import {
 } from './Header.module.scss';
 import { useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { signoutUserAsync } from 'redux/modules/auth';
 import { checkPropTypes } from 'prop-types';
 
-const Header = ({ mode, style, title, href, userData }) => {
+const Header = ({ history, mode, style, title, href, userData }) => {
   const navBgRef = useRef();
   const navRef = useRef();
   const dispatch = useDispatch();
+  // const signedIn = useSelector(state => state.auth.signedIn);
+  const signedIn = true;
 
   const commonProps = {
     secondary: false,
@@ -42,30 +44,47 @@ const Header = ({ mode, style, title, href, userData }) => {
       ? navBgRef.current.setAttribute('aria-hidden', 'true')
       : navBgRef.current.setAttribute('aria-hidden', 'false');
   };
-  const handleClickAnyButtons = e => {
+  const handleClickMenuButtons = e => {
     if (e.target.tagName !== 'A') return;
-    navBgRef.current.classList.toggle(activeBg);
-    navRef.current.classList.toggle(activeNav);
-
-    !navBgRef.current.classList.contains(activeBg)
-      ? navBgRef.current.setAttribute('aria-hidden', 'true')
-      : navBgRef.current.setAttribute('aria-hidden', 'false');
+    navBgRef.current.classList.remove(activeBg);
+    navRef.current.classList.remove(activeNav);
   };
   const handleClickSignout = () => {
     dispatch(signoutUserAsync());
+
+    navBgRef.current.classList.remove(activeBg);
+    navRef.current.classList.remove(activeNav);
+
+    history.push('/');
+  };
+  const hadleClickGoBackButton = () => {
+    history.goBack();
+  };
+  const handleClickLogo = () => {
+    signedIn ? history.push('/main') : history.push('/');
   };
 
   return (
     <div className={header}>
-      <Button {...commonProps} value={'Go back button'}>
+      <Button
+        {...commonProps}
+        value={'Go back button'}
+        onClick={hadleClickGoBackButton}
+      >
         <Icon shape={'back'} />
       </Button>
-      <Button {...commonProps} value={'Logo button, goes to HomePage'}>
+      <Button
+        {...commonProps}
+        value={'Logo button, goes to HomePage'}
+        onClick={handleClickLogo}
+      >
         <Logo style={style} title={title} href={href} />
       </Button>
-      <Button {...commonProps} value={'Menu button'} onClick={handleNavMenu}>
-        <Icon shape={'menu'} />
-      </Button>
+      {signedIn && (
+        <Button {...commonProps} value={'Menu button'} onClick={handleNavMenu}>
+          <Icon shape={'menu'} />
+        </Button>
+      )}
       <div className={navBackground} ref={navBgRef}>
         <section className={nav} ref={navRef}>
           <div className={menu}>
@@ -73,22 +92,17 @@ const Header = ({ mode, style, title, href, userData }) => {
             <Navigation
               label={'메뉴 바'}
               className={navMenu}
-              onClick={handleClickAnyButtons}
+              onClick={handleClickMenuButtons}
             />
           </div>
-          <Link
-            to="/"
-            component={() => (
-              <Button
-                {...commonProps}
-                value={'logout button'}
-                className={signoutButton}
-                onClick={handleClickSignout}
-              >
-                <Tag type={'signout'} content={'sign out'} />
-              </Button>
-            )}
-          />
+          <Button
+            {...commonProps}
+            value={'logout button'}
+            className={signoutButton}
+            onClick={handleClickSignout}
+          >
+            <Tag type={'signout'} content={'sign out'} />
+          </Button>
           <Button
             {...commonProps}
             value={'side menu bar close'}
