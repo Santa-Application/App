e11f4bef0c7b4d3f4db126953d785a3f63325247
ejinaaa <eunjink0423@gmail.com>
2021-04-27 04:18:53 +0900

    fix: Fix bugs and restyle in menu tab component

--- client/src/components/MenuTab/MenuTab.js
+++ client/src/components/MenuTab/MenuTab.js
@@ -1,4 +1,4 @@
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { array, string, number } from 'prop-types';
 import { menuTab } from './MenuTab.module.scss';
 
@@ -6,17 +6,22 @@ const MenuTab = ({ menus, label, selected, onClick }) => {
   return (
     <ul role="tablist" aria-label={label} className={menuTab}>
       {menus.map((menu, index) => (
-        <li
-          key={menu.name}
-          role="tab"
-          aria-selected={index === selected ? 'true' : 'false'}
-          aria-controls={`${menu?.name}-tab`} // should be associated with aria-labelledby
-          tabIndex={index === 0 ? '0' : '-1'}
-          id={menu?.name}
-          onClick={onClick}
-        >
-          <NavLink to={menu?.path} activeClassName="selected">
-            {menu?.name}
+        // <li
+        //   key={menu.name}
+        //   role="tab"
+        //   aria-selected={index === selected ? 'true' : 'false'}
+        //   aria-controls={`${menu?.name}-tab`} // should be associated with aria-labelledby
+        //   tabIndex={index === 0 ? '0' : '-1'}
+        //   id={menu?.name}
+        //   onClick={onClick}
+        // >
+        //   <NavLink to={menu?.path} activeClassName="selected">
+        //     {menu?.name}
+        //   </NavLink>
+        // </li>
+        <li key={index}>
+          <NavLink to={menu.path} className="tab" activeClassName="tabSelected">
+            {menu.name}
           </NavLink>
         </li>
       ))}
