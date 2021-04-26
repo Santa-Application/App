1f531dc15954f798b2611f478c5dd96fda04c478
Bernese-Corgi <jnyng0621@gmail.com>
2021-04-26 15:31:57 +0900

    feat: Add modules in pages/index.js.

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,6 +2,9 @@ export { default as HomePage } from 'pages/HomePage/HomePage';
 export { default as Register } from 'pages/Register/Register';
 export { default as Login } from 'pages/Login/Login';
 
+export { default as Main } from 'pages/Main/Main';
+export { default as Mountain } from 'pages/Mountain/Mountain';
+
 export { default as RecruitList } from 'pages/Recruit/RecruitList';
 export { default as RecruitDetail } from 'pages/Recruit/RecruitDetail';
 export { default as RecruitEdit } from 'pages/Recruit/RecruitEdit';
@@ -12,6 +15,6 @@ export { default as RegularDetail } from 'pages/Regular/RegularDetail';
 export { default as RegularEdit } from 'pages/Regular/RegularEdit';
 export { default as RegularCreate } from 'pages/Regular/RegularCreate';
 
-export { default as UserRecruitList } from 'pages/Profile/UserRecruitList';
+// export { default as UserRecruitList } from 'pages/Profile/UserRecruitList';
 
 export { default as PageNotFound } from 'pages/PageNotFound/PageNotFound';
