86605bb3b6c5f43aa67e9bef4fedcfb05ef59f41
Bernese-Corgi <jnyng0621@gmail.com>
2021-04-27 12:40:27 +0900

    feat: Add search section in Main page.

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,7 +1,14 @@
+import MainRecruit from 'containers/MainRecruit/MainRecruit';
 import SearchMountain from 'containers/SearchMountain/SearchMountain';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMountainAsync } from 'redux/modules/mountain';
+import {
+  mainPage,
+  searchSection,
+  searchContainer,
+  headMessage,
+} from './Main.module.scss';
 
 const Main = ({ history, match }) => {
   const mountain = useSelector(state => state.mountain);
@@ -10,16 +17,24 @@ const Main = ({ history, match }) => {
 
   useEffect(() => {
     dispatch(getMountainAsync());
-  }, [dispatch]);
+  }, []);
+
   return (
-    <main>
-      <SearchMountain
-        history={history}
-        match={match}
-        isLoading={isLoading}
-        data={data}
-        error={error}
-      />
+    <main className={mainPage}>
+      <div className={searchSection}>
+        <SearchMountain
+          history={history}
+          match={match}
+          isLoading={isLoading}
+          data={data}
+          error={error}
+        />
+        <div className={headMessage}>
+          <p>오늘 기분타?</p>
+          <p>그럼 산타!</p>
+        </div>
+      </div>
+      <MainRecruit />
     </main>
   );
 };
