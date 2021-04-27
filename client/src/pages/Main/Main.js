1d9d611dd099985fc533d315919ac80587e79c45
Bernese-Corgi <jnyng0621@gmail.com>
2021-04-27 12:39:45 +0900

    feat: Modify dispatch location in Mountain page.

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,10 +1,25 @@
 import SearchMountain from 'containers/SearchMountain/SearchMountain';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMountainAsync } from 'redux/modules/mountain';
 
 const Main = ({ history, match }) => {
+  const mountain = useSelector(state => state.mountain);
+  const { isLoading, data, error } = mountain;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMountainAsync());
+  }, [dispatch]);
   return (
     <main>
-      <SearchMountain history={history} match={match} />
+      <SearchMountain
+        history={history}
+        match={match}
+        isLoading={isLoading}
+        data={data}
+        error={error}
+      />
     </main>
   );
 };
--- client/src/pages/Mountain/Mountain.js
+++ client/src/pages/Mountain/Mountain.js
@@ -1,11 +1,40 @@
-import { Heading } from 'components';
+import { Heading, MenuTab } from 'components';
 import MountainOverview from 'containers/MountainOverview/MountainOverview';
-import React from 'react';
+import { motion, useTransform, useViewportScroll } from 'framer-motion';
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  mountainImage,
+  mountainPage,
+  mountainHeading,
+  mountainTabContainer,
+} from './Mountain.module.scss';
 
 const Mountain = ({ history, match }) => {
+  // mountain data dispatch
+  const mountain = useSelector(state => state.mountain);
+  const { isLoading, data, error } = mountain;
+
+  const mountainName = match.params.name;
+  const mountainData = data.find(_data => _data.data.name === mountainName);
+  const { imageURL } = mountainData;
   return (
-    <main>
-      <MountainOverview history={history} match={match} />
+    <main className={mountainPage}>
+      <div className={mountainImage}>
+        <img src={imageURL} alt="" />
+        <Heading className={mountainHeading} content={match.params.name} />
+      </div>
+      <div className={mountainTabContainer}>
+        {/* <MenuTab /> */}
+        <MountainOverview
+          history={history}
+          match={match}
+          mountainData={mountainData}
+          isLoading={isLoading}
+          data={data}
+          error={error}
+        />
+      </div>
     </main>
   );
 };
