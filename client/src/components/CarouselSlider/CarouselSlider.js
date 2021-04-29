29f8c994346f50a6a9f32478c1487f22bf04eb66
ejinaaa <eunjink0423@gmail.com>
2021-04-30 01:06:03 +0900

    feat: Add key prop to each slider card component

--- client/src/components/CarouselSlider/CarouselSlider.js
+++ client/src/components/CarouselSlider/CarouselSlider.js
@@ -1,8 +1,6 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import {
-  card
-} from './CarouselSlider.module.scss';
+import { card } from './CarouselSlider.module.scss';
 import { useEffect } from 'react';
 import { bool, array, oneOfType, number, string } from 'prop-types';
 
@@ -18,30 +16,34 @@ const CarouselSlider = ({
   showThumbs,
   ...restProps
 }) => {
-
   const handleLoadCarouselEnhanceAccessibility = () => {
     const $lis = document.querySelectorAll('li.dot');
     const $liItems = document.querySelectorAll('li.slide');
     const $aItems = document.querySelectorAll('li.slide > a');
 
     $lis.forEach($li => $li.removeAttribute('role'));
-    $aItems.forEach(($a, index) => $a.setAttribute('aria-label', `${index + 1} of ${$liItems.length}`));
+    $aItems.forEach(($a, index) =>
+      $a.setAttribute('aria-label', `${index + 1} of ${$liItems.length}`)
+    );
   };
 
   useEffect(() => {
     window.addEventListener('load', handleLoadCarouselEnhanceAccessibility);
 
     return () => {
-      window.removeEventListener('load', handleLoadCarouselEnhanceAccessibility);
+      window.removeEventListener(
+        'load',
+        handleLoadCarouselEnhanceAccessibility
+      );
     };
   }, []);
 
   return (
-    <Carousel 
-      emulateTouch={emulateTouch} 
-      autoPlay={autoPlay} 
-      centerMode={centerMode} 
-      infiniteLoop={infiniteLoop} 
+    <Carousel
+      emulateTouch={emulateTouch}
+      autoPlay={autoPlay}
+      centerMode={centerMode}
+      infiniteLoop={infiniteLoop}
       showArrows={showArrows}
       useKeyboardArrows={useKeyboardArrows}
       width={width}
@@ -49,16 +51,14 @@ const CarouselSlider = ({
       className={card}
       labels={{
         leftArrow: 'previous slide / item',
-        rightArrow: 'next slide / item', 
-        item: 'slide item'
+        rightArrow: 'next slide / item',
+        item: 'slide item',
       }}
       {...restProps}
     >
-      {
-        slides.map(slide => {
-          return slide;
-        })
-      }
+      {slides.map((slide, index) => {
+        return <div key={index}>{slide}</div>;
+      })}
     </Carousel>
   );
 };
