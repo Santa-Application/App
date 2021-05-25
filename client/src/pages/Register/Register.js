cda9e9f71422d83a7dadbeae95c4a6ab14b3e79a
ejinaaa <eunjink0423@gmail.com>
2021-05-25 23:31:01 +0900

    feat: Remove useless variables

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,14 +1,11 @@
 import { RegisterForm } from 'containers';
 import { Heading } from 'components';
-import { useHistory } from 'react-router-dom';
 
 const Register = () => {
-  const history = useHistory();
-
   return (
     <main>
       <Heading content="sign up" />
-      <RegisterForm history={history} />
+      <RegisterForm />
     </main>
   );
 };
