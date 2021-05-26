a82f7b12e988e71344e9ca91ba84e0079ebcd5cf
ejinaaa <eunjink0423@gmail.com>
2021-05-27 02:09:30 +0900

    docs: Rename variables in usePostDetail custom hook

--- client/src/Hooks/usePostDetail.js
+++ client/src/Hooks/usePostDetail.js
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
+
 import {
   removeRecruitPostAsync,
   toggleApplyRecruitingAsync,
@@ -8,7 +9,7 @@ import {
 import { removeRegularPostAsync } from 'redux/modules/regularPost';
 import { createFormPagePath, createListPagePath } from 'utils/path';
 
-const postType = {
+const post = {
   recruit: 'recruit',
   reviews: 'regular',
 };
@@ -18,15 +19,18 @@ const removeHandler = {
 };
 
 const usePostDetail = (pageInfo, isApplied, setIsApplied) => {
-  const post = `${postType[pageInfo.postType]}Post`;
+  const postType = pageInfo.postType;
+  const postDataName = `${post[postType]}Post`;
 
   const dispatch = useDispatch();
   const history = useHistory();
   const params = useParams();
   const postId = params.postId;
   const userId = useSelector(state => state.auth.userInfo._id);
-  const postsData = useSelector(state => state[post]);
-  const postData = postsData.data.find(data => data[post]._id === postId);
+  const postsData = useSelector(state => state[postDataName]);
+  const postData = postsData.data.find(
+    data => data[postDataName]._id === postId
+  );
   const publisherId = postData.publisherInfo._id;
   const isUserPublisher = publisherId === userId;
 
@@ -35,17 +39,17 @@ const usePostDetail = (pageInfo, isApplied, setIsApplied) => {
   }, [pageInfo, postId, history]);
 
   const handleClickRemovePost = useCallback(async () => {
-    const removePostAsync = removeHandler[pageInfo.postType];
+    const removePostAsync = removeHandler[postType];
     await dispatch(removePostAsync(postId));
     history.push(createListPagePath(pageInfo));
-  }, [pageInfo, postId, history, dispatch]);
+  }, [pageInfo, postId, postType, history, dispatch]);
 
   const handlers = {
     handleClickEditPost,
     handleClickRemovePost,
   };
 
-  if (pageInfo.postType === 'recruit') {
+  if (postType === 'recruit') {
     const handleChangeApplyRecruitingButton = async () => {
       await dispatch(toggleApplyRecruitingAsync(postId, userId));
       setIsApplied(!isApplied);
