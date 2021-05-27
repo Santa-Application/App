7449e7a91aa9a183485a79e32b295c51689a8c11
ejinaaa <eunjink0423@gmail.com>
2021-05-28 02:13:35 +0900

    fix: Fix issue not getting regular post data from usePostForm hook

--- client/src/Hooks/usePostForm.js
+++ client/src/Hooks/usePostForm.js
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 
-const usePostForm = isCreateForm => {
+const usePostForm = (pageInfo, isCreateForm) => {
+  const postType = pageInfo.postType;
+  const postDataName = `${postType}Post`;
   const history = useHistory();
   const params = useParams();
   const postId = params.postId;
@@ -16,9 +18,9 @@ const usePostForm = isCreateForm => {
   const isLoggedInUserPage = userName === loggedInUserName;
 
   // 수정 페이지인 경우 필요한 정보
-  const postsData = useSelector(state => state.recruitPost.data);
-  const postData = postsData.find(_data => _data.recruitPost._id === postId);
-  const prevPost = postData?.recruitPost;
+  const postsData = useSelector(state => state[postDataName].data);
+  const postData = postsData.find(_data => _data[postDataName]._id === postId);
+  const prevPost = postData && postData[postDataName];
   const publisherId = postData?.publisherInfo._id;
   const isUserPost = publisherId === loggedInUserId;
 
